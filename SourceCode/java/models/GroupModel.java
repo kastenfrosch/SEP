370d33864b06e08bb27ad2dff706dd3d23c5a963
FlawedBliss <flawed@trashprojects.moe>
2018-10-18 13:09:34 +0200

    Added generated models

---
+++ SourceCode/java/models/GroupModel.java
@@ -0,0 +1,49 @@
+package models;
+
+import com.j256.ormlite.table.DatabaseTable;
+import com.j256.ormlite.field.DatabaseField;
+import java.io.Serializable;
+
+@DatabaseTable(tableName = GroupModel.TABLE_NAME_GROUP)
+public class GroupModel implements Serializable {
+    static final String TABLE_NAME_GROUP = "group";
+    private static final String FIELD_GROUP_GROUP_ID = "group_id";
+    private static final String FIELD_GROUP_NAME = "name";
+    private static final String FIELD_GROUP_GROUPAGE_ID = "groupage_id";
+    private static final String FIELD_GROUP_SEMESTER_ID = "semester_id";
+
+    @DatabaseField(generatedId = true, columnName = FIELD_GROUP_GROUP_ID)
+    private int group_id;
+    @DatabaseField(columnName = FIELD_GROUP_NAME)
+    private String name;
+    @DatabaseField(columnName = FIELD_GROUP_GROUPAGE_ID)
+    private int groupage_id;
+    @DatabaseField(columnName = FIELD_GROUP_SEMESTER_ID)
+    private int semester_id;
+
+    public GroupModel() { }
+
+    public GroupModel(int group_id, String name, int groupage_id, int semester_id) {
+        this.group_id = group_id;
+        this.name = name;
+        this.groupage_id = groupage_id;
+        this.semester_id = semester_id;
+    }
+
+    public int getGroup_id() { return group_id; }
+
+    public void setGroup_id(int group_id) { this.group_id = group_id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public int getGroupage_id() { return groupage_id; }
+
+    public void setGroupage_id(int groupage_id) { this.groupage_id = groupage_id; }
+
+    public int getSemester_id() { return semester_id; }
+
+    public void setSemester_id(int semester_id) { this.semester_id = semester_id; }
+
+}
