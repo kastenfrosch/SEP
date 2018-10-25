5721424c35129bb62fb92bbe1dc161f5324a1277
Johannes Morzeck <johannes.morzeck@gmail.com>
2018-10-25 14:21:26 +0200

    update on group controller and group fxml

--- SourceCode/java/controller/AdditGroupController.java
+++ SourceCode/java/controller/AdditGroupController.java
@@ -12,11 +12,23 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import modal.InfoModal;
-import models.GroupModel;
-import models.GroupageModel;
-import models.SemesterModel;
+import models.Group;
+import models.Groupage;
+import models.Semester;
+
+import java.sql.SQLException;
 
 public class AdditGroupController {
+
+    private DBManager db;
+
+    {
+        try {
+            db = DBManager.getInstance();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     public Text titleText;
@@ -37,7 +49,39 @@ public class AdditGroupController {
     @FXML
     public ComboBox groupageComboBox;
 
+    @FXML
+    public void initialize() {
 
+        // initializing combobox data
+
+        try {
+
+            ObservableList<String> semesterList = FXCollections.observableArrayList();
+            Dao<Semester, String> semester = db.getSemesterDao();
+
+            for (Semester s : semester.queryForAll()) {
+                semesterList.add(s.getDescription());
+            }
+
+            semesterComboBox.setItems(semesterList);
+
+
+            ObservableList<String> groupageList = FXCollections.observableArrayList();
+            Dao<Groupage, Integer> groupage = db.getGroupageDao();
+
+            for (Groupage g : groupage.queryForAll()) {
+                groupageList.add(g.getDescription());
+            }
+
+            groupageComboBox.setItems(groupageList);
+
+        } catch (java.sql.SQLException e) {
+            e.printStackTrace();
+        }
+
+        //TODO: check which semester is selected and show possible grpgs accordingly
+
+    }
 
 
     public void groupnameInput(ActionEvent actionEvent) {
@@ -50,6 +94,61 @@ public class AdditGroupController {
     }
 
     public void additGroupCreate(ActionEvent actionEvent) {
+
+        // use all selections and text inputs to create a group.
+
+        // making sure that groupname is not empty.
+        // if not, set groupname to the input.
+        String name;
+        if (groupnameInput.getText() == null || groupnameInput.getText().isBlank()) {
+            InfoModal.show("FEHLER!", null, "Kein Gruppenname eingegeben!");
+        }
+        name = groupnameInput.getText();
+
+        // making sure that groupage is not empty.
+        // if not, set groupage to the selection.
+        String groupageString;
+        if (groupageComboBox.getSelectionModel().getSelectedItem() == null) {
+            InfoModal.show("FEHLER!", null, "Keine Groupage ausgewählt!");
+        }
+        groupageString = (String) groupageComboBox.getSelectionModel().getSelectedItem();
+
+        // making sure that semester is not empty.
+        // if not, set groupage to the selection.
+        String semesterString;
+        if (semesterComboBox.getSelectionModel().getSelectedItem() == null) {
+            InfoModal.show("FEHLER!", null, "Kein Semester ausgewählt");
+        }
+        semesterString = (String) semesterComboBox.getSelectionModel().getSelectedItem();
+
+        try {
+
+            // setting groupage string to the corresponding groupage id
+            Dao<Groupage, Integer> groupageDao = db.getGroupageDao();
+            Groupage groupage = groupageDao.queryForEq(Groupage.FIELD_GROUPAGE_DESCRIPTION, groupageString).get(0);
+
+            // setting semester string to the corresponding semester id
+            Dao<Semester, String> semesterDao = db.getSemesterDao();
+            Semester semester = semesterDao.queryForEq(Semester.FIELD_SEMESTER_DESCRIPTION, semesterString).get(0);
+
+            // creating new group instance
+            models.Group newGroup = new Group();
+
+            // passing variables to the new group instance
+            newGroup.setName(name);
+            newGroup.setSemester(semester);
+            newGroup.setGroupage(groupage);
+
+            // save new group into database
+            Dao<Group, Integer> groupDao = db.getGroupDao();
+            groupDao.create(newGroup);
+
+        } catch (java.sql.SQLException e) {
+            e.printStackTrace();
+        }
+
+        // confirmation message
+        InfoModal.show("Group \"" + name + "\" created!");
 
     }
 
