c4a4853dde552ae60d103c884e0ba10c171ca908
Merlin von Rössing <flawed@trashprojects.moe>
2018-10-26 12:15:12 +0200

    move DBUtils to utils package

--- SourceCode/java/utils/DBUtils.java
+++ SourceCode/java/utils/DBUtils.java
@@ -1,8 +1,9 @@
-package connection;
+package utils;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
+import connection.DBManager;
 import models.*;
 
 import java.sql.SQLException;
