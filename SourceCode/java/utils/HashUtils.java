517989a17fbbb4489151675458dda8573f3def7c
Merlin von Roessing <merlin.vonroessing@yahoo.de>
2018-11-02 18:35:06 +0100

    add javadoc

--- SourceCode/java/utils/HashUtils.java
+++ SourceCode/java/utils/HashUtils.java
@@ -15,16 +15,29 @@ public class HashUtils {
     private static final int HASH_LENGTH = 128;
     private static final SecureRandom rand = new SecureRandom();
 
+    /**
+     *
+     * @return Returns a random byte array with the same length as the hash
+     */
     public static byte[] getRandomSalt() {
         byte[] salt = new byte[HASH_LENGTH/8];
         rand.nextBytes(salt);
         return salt;
     }
 
+    /**
+     * @see HashUtils#hash(char[], byte[])
+     */
     public static byte[] hash(String cleartext, byte[] salt) {
         return hash(cleartext.toCharArray(), salt);
     }
 
+    /**
+     *
+     * @param cleartext The text to gasg
+     * @param salt The salt to use when hashing
+     * @return A byte array which is the result of hashing the cleartext and the key
+     */
     public static byte[] hash(char[] cleartext, byte[] salt) {
         try {
             SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
@@ -36,14 +49,29 @@ public class HashUtils {
         }
     }
 
+    /**
+     * @see HashUtils#verify(char[], byte[], byte[])
+     * **/
     public static boolean verify(String cleartext, String hash, String salt) {
         return verify(cleartext.toCharArray(), fromHex(hash), fromHex(salt));
     }
 
+    /**
+     * Calculates a hash for cleartext and salt and compares it to a given hash
+     * @param cleartext The cleartext to use when calculating the hash
+     * @param hash The hash to compare to
+     * @param salt The salt to use when calculating the hash
+     * @return true if the hash of cleartext+salt equals the given hash
+     */
     public static boolean verify(char[] cleartext, byte[] hash, byte[] salt) {
         return Arrays.equals(hash(cleartext, salt), hash);
     }
 
+    /**
+     * Converts a byte array to a hex string
+     * @param data The bytes to convert
+     * @return A string with the hex value of the byte array
+     */
     public static String toHex(byte[] data) {
         char[] hex = new char[data.length * 2];
 
@@ -56,6 +84,11 @@ public class HashUtils {
         return new String(hex);
     }
 
+    /**
+     * Converts a hex string to a byte array
+     * @param hex The hex string to convert
+     * @return A byte array with the value of the hex string
+     */
     public static byte[] fromHex(String hex) {
         byte[] data = new byte[hex.length() / 2];
 
