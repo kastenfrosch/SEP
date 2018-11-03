37e3901d1dcf5acbbae6903f6e04102392426af9
tobia <tobias.sieber@stud.uni-due.de>
2018-11-03 12:34:02 +0100

    EditStudent implementation of the Errormodel
    EditStudent add the missing comments
    
    EditStudent student change was successful

--- SourceCode/java/controller/EditStudentController.java
+++ SourceCode/java/controller/EditStudentController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import modal.ConfirmationModal;
+import modal.ErrorModal;
 import modal.InfoModal;
 import models.Group;
 import models.Person;
@@ -75,22 +76,22 @@ public class EditStudentController {
         // initializing combobox data
 
         try {
-
+            // creat an observableList with all semesters
             ObservableList<Semester> semesterList = FXCollections.observableArrayList();
             Dao<Semester, String> semesterDao = dbManager.getSemesterDao();
-
             semesterList.addAll(semesterDao.queryForAll());
-
+            //set semester combobox with the semester from the observableList
             semesterComboBox.setItems(semesterList);
 
+            // creat an observableList with all groups
             ObservableList<Group> groupList = FXCollections.observableArrayList();
             Dao<Group, Integer> groupDao = dbManager.getGroupDao();
-
             groupList.addAll(groupDao.queryForAll());
-
+            //set semester combobox with the semester from the observableList
             groupComboBox.setItems(groupList);
 
         } catch (java.sql.SQLException e) {
+            ErrorModal.show(e.getMessage());
             e.printStackTrace();
         }
 
@@ -100,76 +101,87 @@ public class EditStudentController {
     @FXML
     public void onSaveBtnClicked(ActionEvent event) {
 
-
+        // making sure that matNo is not empty.
         if (matNoInput.getText().isEmpty() || matNoInput == null) {
             InfoModal.show("FEHLER!", null, "Keine Matrikelnummer eingegeben!");
             return;
         }
+        // making sure that firstname is not empty.
         if (firstnameInput.getText().isEmpty() || firstnameInput == null) {
             InfoModal.show("FEHLER!", null, "Kein Vornamen eingegeben!");
             return;
 
         }
+        // making sure that lastname is not empty.
         if (lastnameInput.getText().isEmpty() || lastnameInput == null) {
             InfoModal.show("FEHLER!", null, "Kein Nachnamen eingegeben!");
             return;
         }
+        // making sure that email is not empty & valid.
         if (validateMailAddress(emailInput.toString()) || emailInput == null) {
             InfoModal.show("FEHLER!", null, "E-Mail ist nicht korrekt!");
             return;
         }
+        // making sure that group is not empty.
         if (groupComboBox.getSelectionModel().getSelectedItem() == null) {
             InfoModal.show("FEHLER!", null, "Keine Gruppe ausgewählt!");
             return;
         }
+        // making sure that semester is not empty.
         if (semesterComboBox.getSelectionModel().getSelectedItem() == null) {
             InfoModal.show("FEHLER!", null, "Kein Semester ausgewählt!");
             return;
         }
-
+        //create person & student with the given attributes
         student.setGroup(groupComboBox.getSelectionModel().getSelectedItem());
         student.setSemester(semesterComboBox.getSelectionModel().getSelectedItem());
         student.setMatrNo(matNoInput.getText());
         student.getPerson().setFirstname(firstnameInput.getText());
         student.getPerson().setLastname(lastnameInput.getText());
         student.getPerson().setEmail(emailInput.getText());
-
+    //creating the student & person daos
         Dao<Student, Integer> studentDao = dbManager.getStudentDao();
         Dao<Person, Integer> personDao = dbManager.getPersonDao();
         try {
+            //update the daos with the created person and student
             personDao.update(student.getPerson());
             studentDao.update(student);
+            //notification of the successful change
+            if (this.student.getId() != 0) {
+                InfoModal.show("Der Student \"" + firstnameInput.getText() +" "+ lastnameInput.getText()+ "\" wurde bearbeitet!");}
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+        //back to homeview
         SceneManager.getInstance().closeWindow(SceneManager.SceneType.EDIT_STUDENT);
     }
 
     @FXML
+    //back to homeview
     public void onCancelBTNClicked(ActionEvent event) {
         SceneManager.getInstance().closeWindow(SceneManager.SceneType.EDIT_STUDENT);
     }
 
     @FXML
     public void onDeleteBTNClicked(ActionEvent event) {
-
+        //check if sure
         boolean confirm = ConfirmationModal.show("Soll der Student wirklich gelöscht werden?");
         if(!confirm){
             return;
         }
 
-
         Dao<Student, Integer> studentDao = dbManager.getStudentDao();
         try {
+            // delete the student
             studentDao.delete(student);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        //back to homwview
         SceneManager.getInstance().closeWindow(SceneManager.SceneType.EDIT_STUDENT);
 
     }
-
+    //Validate the Mail Address by using the javaax.mail InternetAddress object.
     private boolean validateMailAddress(String adr) {
         try {
             InternetAddress a = new InternetAddress(adr);
@@ -179,6 +191,7 @@ public class EditStudentController {
         return true;
     }
 
+//Set the student
     public void setStudent(Student student) {
         this.student = student;
         lastnameInput.setText(student.getPerson().getLastname());
