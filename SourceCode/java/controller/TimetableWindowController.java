dc3610b1a35aa2823fd76260eb0bf922d36ca723
Merlin von Rössing <flawed@trashprojects.moe>
2018-11-14 18:42:08 +0100

    removed unused imports

--- SourceCode/java/controller/TimetableWindowController.java
+++ SourceCode/java/controller/TimetableWindowController.java
@@ -1,32 +1,16 @@
 package controller;
 
-import com.j256.ormlite.dao.Dao;
-import connection.DBManager;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import modal.ErrorModal;
-import modal.InfoModal;
-import models.*;
-import utils.SceneManager;
-import javafx.fxml.FXML;
 
 public class TimetableWindowController {
 
-	@FXML
-	public GridPane gridPane;
-	public Label m1;
-	public Label m2;
-	public Label m3;
+    @FXML
+    public GridPane gridPane;
+    public Label m1;
+    public Label m2;
+    public Label m3;
 //	
 //	
 //	@FXML
@@ -35,6 +19,6 @@ public class TimetableWindowController {
 //		//m1.setText("GruppeA & B");
 //		
 //	}
-	
-	
+
+
 }
