140e90ed39ed492870dcc7bdb76e5114e9b2d309
Kaan Erdogan <kaan.erdogan@stud.uni-due.de>
2018-11-19 12:58:35 +0100

    Notepad Model updated

--- SourceCode/java/models/Notepad.java
+++ SourceCode/java/models/Notepad.java
@@ -3,14 +3,13 @@ package models;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
-// TODO: 12.11.2018  Field_Notepad_Priority ergänzen
-
 @DatabaseTable(tableName = Notepad.TABLE_NOTEPAD)
 public class Notepad {
 
     public static final String TABLE_NOTEPAD = "Notepad";
     public static final String FIELD_NOTEPAD_ID = "Notepad_id";
     public static final String FIELD_NOTEPAD_PRIORITY = "Notepad_Priority";
+    public static final String FIELD_NOTEPAD_CONTENT = "Notepad_Content";
     public static final String FIELD_USERNAME = User.FIELD_USERNAME;
 
     @DatabaseField(generatedId = true, columnName = FIELD_NOTEPAD_ID)
@@ -18,6 +17,9 @@ public class Notepad {
 
     @DatabaseField(columnName = FIELD_NOTEPAD_PRIORITY)
     private String notepadPriority;
+
+    @DatabaseField(columnName = FIELD_NOTEPAD_CONTENT)
+    private String notepadContent;
 
     @DatabaseField(columnName = FIELD_USERNAME, foreign = true, foreignAutoRefresh = true,
             columnDefinition = "varchar not null references \"user\"(username)")
@@ -28,6 +30,9 @@ public class Notepad {
 
     public String getNotepadPriority() {return notepadPriority;}
     public void setNotepadPriority(String notepadPriority) {this.notepadPriority = notepadPriority;}
+
+    public String getNotepadContent() {return notepadContent;}
+    public void setNotepadContent(String notepadContent) {this.notepadContent = notepadContent;}
 
     public User getUser() {return user;}
     public void setUser(User user) {this.user = user;}
