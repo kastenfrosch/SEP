942614b2657e2b5e2024eb905a9f26153c3b9e50
Kaan Erdogan <kaan.erdogan@stud.uni-due.de>
2018-11-19 12:30:58 +0100

    Notepad Model updated

--- SourceCode/java/models/Notepad.java
+++ SourceCode/java/models/Notepad.java
@@ -10,23 +10,25 @@ public class Notepad {
 
     public static final String TABLE_NOTEPAD = "Notepad";
     public static final String FIELD_NOTEPAD_ID = "Notepad_id";
-  //  public static final String FIELD_NOTEPAD_PRIORITY = "Notepad_Priority";
+    public static final String FIELD_NOTEPAD_PRIORITY = "Notepad_Priority";
     public static final String FIELD_USERNAME = User.FIELD_USERNAME;
 
     @DatabaseField(generatedId = true, columnName = FIELD_NOTEPAD_ID)
     private int notepadId;
+
+    @DatabaseField(columnName = FIELD_NOTEPAD_PRIORITY)
+    private String notepadPriority;
 
     @DatabaseField(columnName = FIELD_USERNAME, foreign = true, foreignAutoRefresh = true,
             columnDefinition = "varchar not null references \"user\"(username)")
     private User user;
 
     public int getNotepadId() {return notepadId;}
+    public void setNotepadId(int notepadId) {this.notepadId = notepadId;}
 
-    public User getUser() {
-        return user;
-    }
+    public String getNotepadPriority() {return notepadPriority;}
+    public void setNotepadPriority(String notepadPriority) {this.notepadPriority = notepadPriority;}
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+    public User getUser() {return user;}
+    public void setUser(User user) {this.user = user;}
 }
