13ecf6a970a75f8815176bc820276b99ec3d2957
Merlin von Rössing <flawed@trashprojects.moe>
2018-11-22 12:33:22 +0100

    add independent id to favourite models ormlite does not work well with multi-field pkeys add daos for favourites

--- SourceCode/java/models/FavouriteGroup.java
+++ SourceCode/java/models/FavouriteGroup.java
@@ -7,14 +7,19 @@ import com.j256.ormlite.table.DatabaseTable;
 @DatabaseTable(tableName = FavouriteGroup.TABLE_FAV_GROUP)
 public class FavouriteGroup {
     public static final String TABLE_FAV_GROUP = "favourite_semester";
+    //group_id / user_id would be better but ormlite and multi-field keys dont like each other
+    public static final String FIELD_FAVOURITE_ID = "favourite_id";
     public static final String FIELD_GROUP_ID = "group_id";
     public static final String FIELD_USER_ID = "user_id";
 
-    @DatabaseField(columnName = FIELD_GROUP_ID, id = true, foreign = true, foreignAutoRefresh = true,
+    @DatabaseField(columnName = FIELD_FAVOURITE_ID, generatedId = true)
+    private int id;
+
+    @DatabaseField(columnName = FIELD_GROUP_ID, foreign = true, foreignAutoRefresh = true,
             columnDefinition = "integer not null references group(group_id)")
     private int groupId;
 
-    @DatabaseField(columnName = FIELD_USER_ID, id = true, foreign = true, foreignAutoRefresh = true,
+    @DatabaseField(columnName = FIELD_USER_ID, foreign = true, foreignAutoRefresh = true,
             columnDefinition = "integer not null references \"user\"(user_id)")
     private int userId;
 
@@ -32,5 +37,9 @@ public class FavouriteGroup {
 
     public void setUserId(int userId) {
         this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
     }
 }
