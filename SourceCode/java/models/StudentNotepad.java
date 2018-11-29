f4977c046a3d09df9740f313659bdbabf4c61836
Merlin von Roessing <merlin.vonroessing@yahoo.de>
2018-11-29 23:15:03 +0100

    add id field while user / entity id could be used as PK, this doesn't work well with ormlite

--- SourceCode/java/models/StudentNotepad.java
+++ SourceCode/java/models/StudentNotepad.java
@@ -8,13 +8,17 @@ public class StudentNotepad {
     public static final String TABLE_STUDENT_NOTEPAD = "notepad_student";
     public static final String FIELD_NOTEPAD_ID = "notepad_id";
     public static final String FIELD_USERNAME = "username";
+    public static final String FIELD_COMBO_ID = "combo_id";
+
+    @DatabaseField(columnName = FIELD_COMBO_ID, generatedId = true)
+    private int id;
 
     @DatabaseField(columnName = FIELD_NOTEPAD_ID, foreign = true, foreignAutoRefresh = true,
-            columnDefinition = "integer not null references notepad(notepad_id)", id = true)
+            columnDefinition = "integer not null references notepad(notepad_id)", uniqueCombo = true)
     private Notepad notepad;
 
     @DatabaseField(columnName = FIELD_USERNAME, foreign = true, foreignAutoRefresh = true,
-            columnDefinition = "varchar not null references student(student_id)", id = true)
+            columnDefinition = "varchar not null references student(student_id)", uniqueCombo = true)
     private Student student;
 
     public Notepad getNotepad() {
@@ -31,5 +35,9 @@ public class StudentNotepad {
 
     public void setStudent(Student student) {
         this.student = student;
+    }
+
+    public int getId() {
+        return id;
     }
 }
