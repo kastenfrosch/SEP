a607ae8cf04e5c74320cd368c19363d68f04c244
Merlin von Rössing <flawed@trashprojects.moe>
2018-11-29 13:49:04 +0100

    add toSimpleDateString

--- SourceCode/java/utils/TimeUtils.java
+++ SourceCode/java/utils/TimeUtils.java
@@ -5,11 +5,19 @@ import java.time.format.DateTimeFormatter;
 
 public class TimeUtils {
     private static DateTimeFormatter simpleDtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static DateTimeFormatter simpleDateDtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public static String toSimpleString(LocalDateTime ldt) {
         if(ldt == null) {
             return "0000-00-00 00:00:00";
         }
         return ldt.format(simpleDtf);
+    }
+
+    public static String toSimpleDateString(LocalDateTime ldt) {
+        if(ldt == null) {
+            return "0000-00-00";
+        }
+        return ldt.format(simpleDateDtf);
     }
 }
