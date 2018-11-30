38403cebd7b05f4b5e1063352aa1c285d9b43e9c
Merlin von Roessing <merlin.vonroessing@yahoo.de>
2018-11-30 20:02:25 +0100

    update tardy to specifications

--- SourceCode/java/controller/TardyController.java
+++ SourceCode/java/controller/TardyController.java
@@ -2,12 +2,10 @@ package controller;
 
 import com.j256.ormlite.dao.Dao;
 import connection.DBManager;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -16,121 +14,104 @@ import javafx.scene.text.Text;
 import javafx.util.StringConverter;
 import javafx.util.converter.IntegerStringConverter;
 import modal.ErrorModal;
+import models.CalendarEntry;
 import models.Student;
 import models.Tardy;
-import utils.TimeUtils;
 
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.util.stream.Collectors;
 
 public class TardyController {
     @FXML
     private Text titleText;
     @FXML
-    private Button newBtn;
-    @FXML
     private TableView<Tardy> tableView;
-    @FXML
-    private Button deleteBtn;
 
-    private Student student;
+    private CalendarEntry calendarEntry;
     private Dao<Tardy, Integer> tardyDao;
 
     @FXML
-    public void initialize()
-    {
+    public void initialize() {
         try {
             tardyDao = DBManager.getInstance().getTardyDao();
-        } catch(SQLException ex) {
+        } catch (SQLException ex) {
             ErrorModal.show("Fehlzeiten konnten nicht geladen werden.");
             return;
         }
-        TableColumn<Tardy, LocalDate> dateCol = new TableColumn<>("Datum");
+
+        this.tableView.getColumns().clear();
+        /**/
+        TableColumn<Tardy, String> firstnameCol = new TableColumn<>("Vorname");
+        TableColumn<Tardy, String> lastnameCol = new TableColumn<>("Nachname");
+        TableColumn<Tardy, Boolean> presentCol = new TableColumn<>("Anwesend");
         TableColumn<Tardy, Integer> timeCol = new TableColumn<>("Fehlzeit (min)");
-        TableColumn<Tardy, Boolean> excuseCol = new TableColumn<>("Entschuldigt");
+        TableColumn<Tardy, Boolean> excusedCol = new TableColumn<>("Entschuldigt");
 
-
-        dateCol.setCellValueFactory(new PropertyValueFactory<>("localDateMissed"));
+        firstnameCol.setCellValueFactory(s -> new SimpleStringProperty(s.getValue().getStudent().getPerson().getFirstname()));
+        lastnameCol.setCellValueFactory(s -> new SimpleStringProperty(s.getValue().getStudent().getPerson().getLastname()));
+        presentCol.setCellValueFactory(new PropertyValueFactory<>("present"));
         timeCol.setCellValueFactory(new PropertyValueFactory<>("timeMissed"));
-        excuseCol.setCellValueFactory(new PropertyValueFactory<>("excused"));
+        excusedCol.setCellValueFactory(new PropertyValueFactory<>("excused"));
 
-        dateCol.setCellFactory(TextFieldTableCell.forTableColumn(new StringConverter<>() {
-            @Override
-            public String toString(LocalDate localDate) {
-                //TODO why is localDate null when a new Tardy is created
-                if(localDate == null) {
-                    return "";
-                }
-                return TimeUtils.toSimpleDateString(LocalDateTime.of(localDate, LocalTime.of(0, 0, 0)));
-            }
-
-            @Override
-            public LocalDate fromString(String s) {
-                return TimeUtils.localDateFromString(s);
-            }
-        }));
+        //student name shouldn't be editable so there is no need for a textfieldtablecell
+        presentCol.setCellFactory(TextFieldTableCell.forTableColumn(new YesNoConverter()));
         timeCol.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
-        excuseCol.setCellFactory(TextFieldTableCell.forTableColumn(new StringConverter<>() {
-            @Override
-            public String toString(Boolean aBoolean) {
-                if (aBoolean) {
-                    return "Ja";
-                }
-                return "Nein";
-            }
+        excusedCol.setCellFactory(TextFieldTableCell.forTableColumn(new YesNoConverter()));
 
-            @Override
-            public Boolean fromString(String s) {
-                if (s.equals("Ja")) {
-                    return true;
-                }
-                return false;
-            }
-        }));
+        presentCol.setOnEditCommit(event -> {
+            try {
+                Tardy t = tableView.getSelectionModel().getSelectedItem();
+                t.setPresent(event.getNewValue());
 
+                tardyDao.update(t);
+            } catch (SQLException ex) {
+                event.consume();
+                ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden.");
+            }
+        });
         timeCol.setOnEditCommit(event -> {
             try {
                 Tardy t = tableView.getSelectionModel().getSelectedItem();
                 t.setTimeMissed(event.getNewValue());
 
                 tardyDao.update(t);
-            } catch(SQLException ex) {
+            } catch (SQLException ex) {
                 event.consume();
                 ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden.");
             }
         });
-        excuseCol.setOnEditCommit(event -> {
+        excusedCol.setOnEditCommit(event -> {
             try {
 
                 Tardy t = tableView.getSelectionModel().getSelectedItem();
                 t.setExcused(event.getNewValue());
 
                 tardyDao.update(t);
-            } catch(SQLException ex) {
+            } catch (SQLException ex) {
                 event.consume();
                 ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden.");
             }
         });
 
-        dateCol.setEditable(true);
+        firstnameCol.setEditable(false);
+        lastnameCol.setEditable(false);
+        presentCol.setEditable(true);
         timeCol.setEditable(true);
-        excuseCol.setEditable(true);
+        excusedCol.setEditable(true);
 
-        tableView.getColumns().addAll(dateCol, timeCol, excuseCol);
+        tableView.getColumns().addAll(firstnameCol, lastnameCol, presentCol, timeCol, excusedCol);
 
         refreshList();
     }
 
 
-    public void setStudent(Student student) {
-        this.student = student;
+    public void setArgs(CalendarEntry entry) {
+        this.calendarEntry = entry;
 
         this.titleText.setText(
                 String.format(
                         this.titleText.getText(),
-                        student.getPerson().getFirstname()
+                        calendarEntry.getCalendar().getGroupage().getDescription()
                 )
         );
 
@@ -142,49 +123,80 @@ public class TardyController {
             DBManager db = DBManager.getInstance();
             Dao<Tardy, Integer> dao = db.getTardyDao();
             //TODO: remove before release, debug only
-            if (student == null) {
-                Dao<Student, Integer> sDao = db.getStudentDao();
-                this.student = sDao.queryForId(1);
+            if (this.calendarEntry == null) {
+                Dao<CalendarEntry, Integer> sDao = db.getCalendarEntryDao();
+                this.calendarEntry = sDao.queryForId(1);
             }
 
-            ObservableList<Tardy> tardies = FXCollections.observableArrayList(
-                    dao.queryForEq(Tardy.FIELD_STUDENT, this.student.getId())
-            );
+            ObservableList<Student> studentsInGroupage = FXCollections.observableArrayList();
+            this.calendarEntry.
+                    getCalendar().
+                    getGroupage().
+                    getGroups().
+                    forEach(group -> {
+                        studentsInGroupage.addAll(group.getStudents());
+                    });
 
+            ObservableList<Tardy> listItems = FXCollections.observableArrayList();
 
-            this.tableView.setItems(tardies);
+            boolean entryExists = tardyDao.query(
+                    tardyDao.queryBuilder()
+                            .limit(1L)
+                            .where()
+                            .eq(Tardy.FIELD_DATE_MISSED, this.calendarEntry.getEntryId())
+                            .and()
+                            .in(Tardy.FIELD_STUDENT, studentsInGroupage.stream().map(Student::getId).collect(Collectors.toList()))
+                            .prepare()
+            ).size() > 0;
+
+            if (!entryExists) {
+                //no list for this date+class exists yet, so we generate one
+                for (var s : studentsInGroupage) {
+                    Tardy t = new Tardy();
+                    t.setDateMissed(this.calendarEntry);
+                    t.setExcused(false);
+                    t.setPresent(false);
+                    t.setStudent(s);
+                    t.setTimeMissed(0);
+                    tardyDao.create(t);
+                    listItems.add(t);
+                }
+            } else {
+                //add all tardies from students in this class and at this calendarentry to the list
+                listItems.addAll(
+                        tardyDao.query(
+                                tardyDao.queryBuilder()
+                                        .where()
+                                        .in(Tardy.FIELD_STUDENT, studentsInGroupage.stream().map(Student::getId).collect(Collectors.toList()))
+                                        .and()
+                                        .eq(Tardy.FIELD_DATE_MISSED, this.calendarEntry)
+                                        .prepare()
+                        )
+                );
+            }
+
+            this.tableView.setItems(listItems);
 
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
     }
 
-    public void onNewBtnClicked(ActionEvent actionEvent) {
-        Tardy t = new Tardy();
-        t.setLocalDateMissed(LocalDate.now());
-        t.setTimeMissed(0);
-        t.setExcused(false);
-        t.setStudent(student);
-
-        try {
-            tardyDao.create(t);
-            tableView.getItems().add(t);
-            tableView.getSelectionModel().select(t);
-            tableView.edit(tableView.getSelectionModel().getSelectedIndex(), tableView.getColumns().get(0));
-
-        } catch(SQLException ex) {
-            ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden.");
+    private class YesNoConverter extends StringConverter<Boolean> {
+        @Override
+        public String toString(Boolean aBoolean) {
+            if (aBoolean) {
+                return "Ja";
+            }
+            return "Nein";
         }
 
-    }
-
-    public void onDeleteBtnClicked(ActionEvent actionEvent) {
-        Tardy t = tableView.getSelectionModel().getSelectedItem();
-        try {
-            tardyDao.delete(t);
-            tableView.getItems().remove(t);
-        } catch (SQLException e) {
-            ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden.");
+        @Override
+        public Boolean fromString(String s) {
+            if (s.equals("Ja")) {
+                return true;
+            }
+            return false;
         }
     }
 }
