8b871eff0dbdf8fe5687e35fe77d5899e3e3c288
DaveMakila <davemakila@gmx.de>
2018-12-02 17:43:40 +0100

    create and edit termorganisation

--- SourceCode/java/controller/SemesterplanController.java
+++ SourceCode/java/controller/SemesterplanController.java
@@ -2,29 +2,30 @@ package controller;
 
 import com.j256.ormlite.dao.Dao;
 import connection.DBManager;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.text.Text;
-import javafx.util.StringConverter;
 import javafx.util.converter.IntegerStringConverter;
 import modal.ErrorModal;
 import models.Calendar;
 import models.CalendarEntry;
 import models.CalendarExtraInfo;
+import models.Semester;
 import utils.TimeUtils;
-
 import java.sql.SQLException;
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 
 public class SemesterplanController {
 
@@ -35,24 +36,29 @@ public class SemesterplanController {
     @FXML
     public Button deleteBtn;
     @FXML
-    public DatePicker sepDatePicker;
-    @FXML
     public Text titleText;
 
-    private CalendarExtraInfo calendarExtraInfo;
-
     private Dao<CalendarExtraInfo, Integer> calendarExtraInfoDao;
+    private Dao<CalendarEntry, Integer> calendarEntryDao;
+    private Dao<Calendar, Integer> calendarDao;
+
+    private Semester semester;
+    private Calendar calendar;
+
     private DBManager db;
 
-    public void initialize(){
+    public void initialize() {
         try {
-            calendarExtraInfoDao = db.getInstance().getCalendarExtraInfoDao();
-        } catch(SQLException e) {
-            ErrorModal.show("Semesterplan konnten nicht geladen werden.");
+            db = DBManager.getInstance();
+            calendarExtraInfoDao = db.getCalendarExtraInfoDao();
+            calendarEntryDao = db.getCalendarEntryDao();
+            calendarDao = db.getCalendarDao();
+        } catch (SQLException e) {
+            ErrorModal.show("Der Semesterplan konnten nicht geladen werden!");
             return;
         }
 
-        TableColumn<CalendarExtraInfo, LocalDate> sepDatesCol = new TableColumn<>("SEP-Termine");
+        TableColumn<CalendarExtraInfo, String> sepDatesCol = new TableColumn<>("SEP-Termine");
         TableColumn<CalendarExtraInfo, Integer> cwCol = new TableColumn<>("KW");
         TableColumn<CalendarExtraInfo, Integer> meetingNoCol = new TableColumn<>("TerminNr.");
         TableColumn<CalendarExtraInfo, String> iterationCol = new TableColumn<>("Iteration");
@@ -60,31 +66,32 @@ public class SemesterplanController {
         TableColumn<CalendarExtraInfo, String> workingPhaseCol = new TableColumn<>("Arbeitsphase");
 
 
-        sepDatesCol.setCellValueFactory(new PropertyValueFactory<>("calendarEntry"));
+        sepDatesCol.setCellValueFactory(s ->
+                new SimpleStringProperty(TimeUtils.toSimpleDateString(s.getValue().getCalendarEntry().getStartTimeAsLocalDateTime()))
+        );
         cwCol.setCellValueFactory(new PropertyValueFactory<>("calendarWeek"));
         meetingNoCol.setCellValueFactory(new PropertyValueFactory<>("meetingNo"));
         iterationCol.setCellValueFactory(new PropertyValueFactory<>("iterationInfo"));
         lectureContentCol.setCellValueFactory(new PropertyValueFactory<>("lectureInfo"));
         workingPhaseCol.setCellValueFactory(new PropertyValueFactory<>("workphaseInfo"));
 
-        //TODO: sepDatesCol.setCellFactory()
-        sepDatesCol.setCellFactory(TextFieldTableCell.forTableColumn(sepDatePicker.getConverter()));
+
+        sepDatesCol.setCellFactory(TextFieldTableCell.forTableColumn());
         cwCol.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
         meetingNoCol.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
         iterationCol.setCellFactory(TextFieldTableCell.forTableColumn());
         lectureContentCol.setCellFactory(TextFieldTableCell.forTableColumn());
         workingPhaseCol.setCellFactory(TextFieldTableCell.forTableColumn());
 
-        //TODO: sepDatesCol.setOnEditCommit()
         cwCol.setOnEditCommit(event -> {
             try {
                 CalendarExtraInfo table = semesterplan.getSelectionModel().getSelectedItem();
                 table.setCalendarWeek(event.getNewValue());
 
                 calendarExtraInfoDao.update(table);
-            } catch(SQLException e) {
+            } catch (SQLException e) {
                 event.consume();
-                ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden.");
+                ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden!");
             }
         });
         meetingNoCol.setOnEditCommit(event -> {
@@ -93,9 +100,9 @@ public class SemesterplanController {
                 table.setMeetingNo(event.getNewValue());
 
                 calendarExtraInfoDao.update(table);
-            } catch(SQLException e) {
+            } catch (SQLException e) {
                 event.consume();
-                ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden.");
+                ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden!");
             }
         });
         iterationCol.setOnEditCommit(event -> {
@@ -104,9 +111,9 @@ public class SemesterplanController {
                 table.setIterationInfo(event.getNewValue());
 
                 calendarExtraInfoDao.update(table);
-            } catch(SQLException e) {
+            } catch (SQLException e) {
                 event.consume();
-                ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden.");
+                ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden!");
             }
         });
         lectureContentCol.setOnEditCommit(event -> {
@@ -115,9 +122,9 @@ public class SemesterplanController {
                 table.setLectureInfo(event.getNewValue());
 
                 calendarExtraInfoDao.update(table);
-            } catch(SQLException e) {
+            } catch (SQLException e) {
                 event.consume();
-                ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden.");
+                ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden!");
             }
         });
         workingPhaseCol.setOnEditCommit(event -> {
@@ -126,9 +133,27 @@ public class SemesterplanController {
                 table.setWorkphaseInfo(event.getNewValue());
 
                 calendarExtraInfoDao.update(table);
-            } catch(SQLException e) {
+            } catch (SQLException e) {
                 event.consume();
-                ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden.");
+                ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden!");
+            }
+        });
+
+        sepDatesCol.setOnEditCommit(event -> {
+            try {
+                CalendarExtraInfo t = semesterplan.getSelectionModel().getSelectedItem();
+                t.getCalendarEntry().setStartTime(
+                        LocalDateTime.of(
+                                TimeUtils.localDateFromString(event.getNewValue()),
+                                LocalTime.of(0, 0, 0))
+                );
+
+                calendarEntryDao.update(t.getCalendarEntry());
+            } catch (SQLException ex) {
+                ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden!");
+            } catch (DateTimeException ex) {
+                ErrorModal.show("Bitte ein gültiges Dattum (yyyy-MM-dd) eingeben!");
+                semesterplan.refresh();
             }
         });
 
@@ -139,22 +164,20 @@ public class SemesterplanController {
         lectureContentCol.setEditable(true);
         workingPhaseCol.setEditable(true);
 
+
+        semesterplan.getColumns().clear();
         semesterplan.getColumns().addAll(sepDatesCol, cwCol, meetingNoCol, iterationCol, lectureContentCol, workingPhaseCol);
-
-        //TODO: refreshList() Methode fehlt
-//        refreshList();
-    }
-
-    public void chooseDate(ActionEvent event) {
 
     }
 
     public void onAddBtnClicked(ActionEvent event) {
         CalendarExtraInfo c = new CalendarExtraInfo();
         CalendarEntry calendarEntry = new CalendarEntry();
-        calendarEntry.setStartTime(LocalDateTime.of(2018, 10,8,0,0));
+        calendarEntry.setCalendar(this.calendar);
+        calendarEntry.setStartTime(LocalDateTime.of(2018, 10, 8, 0, 0));
+        calendarEntry.setDescription("");
+        calendarEntry.setDayOfWeek(LocalDate.now().getDayOfWeek());
 
-        //TODO: table.setCalendarEntry() hinzufügen bzw. einen richtugen CalendarEntry
         c.setCalendarEntry(calendarEntry);
         c.setCalendarWeek(41);
         c.setMeetingNo(0);
@@ -163,32 +186,16 @@ public class SemesterplanController {
         c.setWorkphaseInfo("Gruppenanmeldung");
 
         try {
+            calendarEntryDao.create(calendarEntry);
             calendarExtraInfoDao.create(c);
             semesterplan.getItems().add(c);
             semesterplan.getSelectionModel().select(c);
             semesterplan.edit(semesterplan.getSelectionModel().getSelectedIndex(), semesterplan.getColumns().get(0));
 
-        } catch(SQLException ex) {
-            ErrorModal.show("Änderungen konnten nicht gespeichert werden.");
-        }
-
-    }
-
-    private void refreshList() {
-        try {
-//            if (calendarExtraInfo == null) {
-//                Dao<CalendarExtraInfo, Integer> calendarExtraInfoDao = db.getCalendarExtraInfoDao();
-//                this.calendarExtraInfo = calendarExtraInfoDao.queryForId(1);
-//            }
-            Dao<CalendarExtraInfo, Integer> dao = db.getCalendarExtraInfoDao();
-            ObservableList<CalendarExtraInfo> plaene = FXCollections.observableArrayList(
-                    dao.queryForEq(CalendarExtraInfo.FIELD_EXTRA_INFO_ID, CalendarEntry.FIELD_ENTRY_ID)
-            );
-            this.semesterplan.setItems(plaene);
-
         } catch (SQLException ex) {
-            ex.printStackTrace();
+            ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden!");
         }
+
     }
 
     public void onDeleteBtnClicked(ActionEvent event) {
@@ -197,7 +204,49 @@ public class SemesterplanController {
             calendarExtraInfoDao.delete(table);
             semesterplan.getItems().remove(table);
         } catch (SQLException e) {
-            ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden.");
+            ErrorModal.show("Ihre Änderungen konnten nicht gespeichert werden!");
         }
+    }
+
+    public void setSemester(Semester semester) {
+        this.semester = semester;
+        loadCalendar();
+    }
+
+    private void loadCalendar() {
+        try {
+
+            List<Calendar> calendars = calendarDao.query(calendarDao.queryBuilder()
+                    .where()
+                    .eq(Calendar.FIELD_CALENDAR_TYPE, Calendar.CalendarType.SEMESTER)
+                    .and()
+                    .eq(Calendar.FIELD_SEMESTER_ID, this.semester.getId()).prepare());
+
+            if (calendars.size() == 0) {
+                Calendar c = new Calendar();
+                c.setSemester(this.semester);
+                c.setCalendarType(Calendar.CalendarType.SEMESTER);
+                calendarDao.create(c);
+                calendarDao.refresh(c);
+                this.calendar = c;
+                return;
+            }
+
+            this.calendar = calendars.get(0);
+
+            ObservableList<CalendarExtraInfo> extraInfo = FXCollections.observableArrayList();
+
+            for (var entry : calendar.getCalendarEntries()) {
+                extraInfo.addAll(entry.getExtraInfo());
+            }
+
+            this.semesterplan.setItems(extraInfo);
+
+
+        } catch (SQLException ex) {
+            ErrorModal.show("Der Semesterplan konnte nicht geladen werden!");
+        }
+
+
     }
 }
