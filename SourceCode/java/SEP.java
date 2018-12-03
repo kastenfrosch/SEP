696e58683decbabaa7937e8dac7c0b8e363635ea
Merlin von Roessing <merlin.vonroessing@yahoo.de>
2018-12-03 01:44:56 +0100

    show login on start

--- SourceCode/java/SEP.java
+++ SourceCode/java/SEP.java
@@ -17,7 +17,7 @@ public class SEP extends Application {
 
         //set primary stage for SceneManager
         SceneManager.getInstance(stage);
-        SceneManager.getInstance().switchTo(SceneType.HOME);
+        SceneManager.getInstance().switchTo(SceneType.LOGIN);
         stage.show();
     }
 }
