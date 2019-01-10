fee78d11049a05896698ddb3392499449952d35b
Merlin von Rössing <flawed@trashprojects.moe>
2019-01-10 12:50:34 +0100

    add mail settings insert settings if missing

--- SourceCode/java/utils/settings/Setting.java
+++ SourceCode/java/utils/settings/Setting.java
@@ -5,7 +5,11 @@ public enum Setting {
     DB_PORT("db_port", "5432"),
     DB_USER("db_user", "sep"),
     DB_PASSWORD("db_password", "ayy1mao"),
-    DB_DB("db_db", "sep2");
+    DB_DB("db_db", "sep2"),
+    SYS_MAIL_HOST("sys_mail_host", "localhost"),
+    SYS_MAIL_SMTP_PORT("sys_mail_smtp_port", "587"),
+    SYS_MAIL_USER("sys_mail_user", "sep@localhost"),
+    SYS_MAIL_PASSWORD("sys_mail_password", "secret");
 
     private String key, defaultValue;
     Setting(String key, String defaultValue) {
--- SourceCode/java/utils/settings/Settings.java
+++ SourceCode/java/utils/settings/Settings.java
@@ -3,8 +3,7 @@ package utils.settings;
 import javafx.application.Platform;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Properties;
+import java.util.*;
 
 public class Settings {
 
@@ -20,29 +19,31 @@ public class Settings {
 
         File f = new File(path);
 
-        if(!f.exists() || f.isDirectory()) {
-            createDefault(path);
-            return;
-        }
-
         try {
-
             props.load(new BufferedReader(new FileReader(f)));
 
+            for(Setting s : Setting.values()) {
+                if(!props.containsKey(s)) {
+                    insertMissingFields(path);
+                }
+            }
+
         } catch(FileNotFoundException ex) {
-            //this should never happen
+            insertMissingFields(path);
         } catch(IOException ex) {
             System.out.println("Failed to load config File: " + ex.getMessage());
             Platform.exit();
         }
     }
 
-    private static void createDefault(String path) {
-        File f = new File(path);
+    private static void insertMissingFields(String path) {
         try {
             for(Setting s : Setting.values()) {
-                props.setProperty(s.getKey(), s.getDefaultValue());
+                if(!props.containsKey(s.getKey())) {
+                    props.setProperty(s.getKey(), s.getDefaultValue());
+                }
             }
+            File f = new File(path);
             props.store(new BufferedWriter(new FileWriter(f)), null);
         } catch(IOException ex) {
             System.out.println(ex.getMessage());
