10fb861b8cc4026b1ed4bc3f161a1144ab23c4f1
Merlin von Roessing <merlin.vonroessing@yahoo.de>
2019-01-13 18:16:41 +0100

    implement all this taco

--- SourceCode/java/controller/gitlab/GitlabLoginController.java
+++ SourceCode/java/controller/gitlab/GitlabLoginController.java
@@ -1,19 +1,24 @@
 package controller.gitlab;
 
+import controller.HomeScreenController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.text.Text;
+import modal.ErrorModal;
 import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
+import utils.scene.SceneManager;
+import utils.scene.SceneType;
 
 public class GitlabLoginController {
+
+    private GitLabApi api;
 
     @FXML
     private AnchorPane rootPane;
@@ -22,18 +27,21 @@ public class GitlabLoginController {
     private Button loginBtn;
 
     @FXML
-    private TextField username;
+    private TextField usernameInput;
 
     @FXML
-    private PasswordField password;
+    private PasswordField passwordInput;
 
     @FXML
-    void onLoginBtnClicked(ActionEvent event) {
+    public void onLoginBtnClicked(ActionEvent event) {
         GitLabApi gitLabApi;
         try {
-           gitLabApi = GitLabApi.oauth2Login("https://git.uni-due.de", username.getText(), password.getText());
+           this.api = GitLabApi.oauth2Login("https://git.uni-due.de", usernameInput.getText(), passwordInput.getText());
+
+            SceneManager.getInstance().getLoaderForScene(SceneType.HOME).<HomeScreenController>getController().refresh();
         } catch (GitLabApiException e) {
-            e.printStackTrace();
+            ErrorModal.show("Login failed", e.getMessage());
+            return;
         }
     }
 
@@ -43,5 +51,9 @@ public class GitlabLoginController {
             this.loginBtn.fire();
             keyEvent.consume();
         }
+    }
+
+    public GitLabApi getApi() {
+        return api;
     }
 }
