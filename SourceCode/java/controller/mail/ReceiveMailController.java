e0131aee1a728c23d85a569ea251efbcb38c06ac
Tobi <tobias.sieber@stud.uni-due.de>
2019-01-21 21:21:06 +0100

    getter for password

--- SourceCode/java/controller/mail/ReceiveMailController.java
+++ SourceCode/java/controller/mail/ReceiveMailController.java
@@ -50,6 +50,11 @@ public class ReceiveMailController {
 
     @FXML
     private TableView<Message> mailTableView;
+
+    public String getMailPassword() {
+        return mailPassword;
+    }
+
     //set properties
     private String mailPassword;
 
