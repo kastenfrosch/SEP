d9327baefdcce84984d6346020fd151193901286
Merlin von Rössing <flawed@trashprojects.moe>
2019-01-24 13:13:46 +0100

    fix db setup options default to login screen

--- SourceCode/java/SEP.java
+++ SourceCode/java/SEP.java
@@ -7,6 +7,7 @@ import utils.scene.SceneManager;
 import utils.scene.SceneType;
 import utils.settings.Settings;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 
@@ -19,31 +20,42 @@ public class SEP extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         Settings.load();
 
+        //make sure we can connect to the db and it exists
         try {
-            DBManager.getInstance();
+            DBManager.getInstance().getSemesterDao().executeRaw("select 1");
         } catch (SQLException ex) {
             System.out.println("ERROR: Die Verbindung zur Datenbank konnte nicht hergestellt werden.\n" +  ex.getMessage());
             Platform.exit();
             return;
         }
 
+        //install the database if requested
         if(System.getProperty("setup") != null) {
             try {
                 DBUtils.resetDB(DBManager.getInstance().getSemesterDao().getConnectionSource(), true);
-            } catch(SQLException ex) {
+            } catch(IOException | SQLException ex) {
                 System.out.println("ERROR: Die Datenbank konnte nicht installiert werden");
             }
             Platform.exit();
             return;
         }
 
-//        DBUtils.resetDB(DBManage.getInstance().getSemesterDao().getConnectionSource(), true);
+        //check if the database is installed
+        try {
+            DBManager.getInstance().getSemesterDao()
+                    .executeRawNoArgs("select semester_id from semester limit 1");
+        } catch(SQLException ex) {
+            System.out.println("ERROR: Die Datenbank ist nicht installiert.\n" + ex.getMessage());
+            Platform.exit();
+            return;
+        }
+
         //set primary stage for SceneManager
         SceneManager.getInstance(stage);
-        SceneManager.getInstance().switchTo(SceneType.NEW_EXAM_GROUP);
+        SceneManager.getInstance().switchTo(SceneType.LOGIN);
         stage.show();
     }
 
